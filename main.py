f6977e30010fe581d0dc8dac8c29f479eb09303d
Anjali2192 <967anjalimishra@gmail.com>
2026-04-20 17:41:23 +0530

    fetched date, type and no of apartments

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ import pandas as pd
 
 df = pd.read_excel("RERA_Mar.xlsx")
 
+async def total_apartments(pg):
+    await pg.wait_for_timeout(2000)
+    cells = await pg.locator("//th[text()='Total']").count()
+    direct = (await pg.locator("//th[text()='Total']/following-sibling::th[3]").inner_text())
+    if cells == 0:
+        return "N/A"
+    else:
+        return direct
+
+async def fetch_data(page, context):
+    await page.locator("//a[text()='View Details']").click()
+    await page.locator("//button[text()='Yes']").wait_for()
+    async with context.expect_page() as new_page_info:
+        await page.locator("//button[text()='Yes']").click()
+    
+    new_page = await new_page_info.value
+    await new_page.wait_for_load_state("domcontentloaded")
+    print(new_page.url)
+    date = (await new_page.locator("//label[text()='Date of Registration']/following-sibling::label[1]").inner_text()).strip()
+    type = (await new_page.locator("//div[text()=' Project Type ']/following-sibling::div[1]").inner_text()).strip()
+    print(date)
+    print(type)
+    apartment = await total_apartments(new_page)
+    print("no: ", apartment)
+
 async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
@@ -23,16 +48,13 @@ async def main():
             await page.locator("input#edit-project-name.leftBdrRadius.form-text").fill(rera_num) 
             await page.wait_for_load_state("load")
             await page.locator("input#edit-submit--2.btn-default.me-2.button.js-form-submit.form-submit").click()
-            await page.locator("//a[text()='View Details']").click()
+            #await page.locator("//a[text()='View Details']").click()
 
             command = await asyncio.to_thread(input, "Enter command: ")
             if command == "next":
-                print("Reached here!")
-                #reset
-                #await page.locator
                 continue
             elif command == "fetch":
-                print(page.url)
+                await fetch_data(page, context)
 
         await context.close()
         await browser.close()
